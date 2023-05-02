44e33ae10984a7f1a8366fca82fb6011168319dc
Tanguy Strub <tanguystrub08@gmail.com>
2023-05-02 17:54:59 +0200

    adding functions to update banner and dots depending on Slide index changing with clicks on arrows

--- assets/script.js
+++ assets/script.js
@@ -17,19 +17,51 @@ const slides = [
 	}
 ]
 
+// Sélectionner les éléments HTML pertinents
+const banner = document.querySelector("#banner");
+const arrowLeft = banner.querySelector(".arrow_left");
+const arrowRight = banner.querySelector(".arrow_right");
+const bannerImg = banner.querySelector(".banner-img");
+const bannerDescription = banner.querySelector("p");
+
 // Initialiser l'index de la diapositive actuelle à 0
 let currentSlideIndex = 0;
 
+// Fonction qui met à jour l'image et la description en fonction de l'index de la diapositive
+function updateBanner() {
+	const currentSlide = slides[currentSlideIndex];
+	bannerImg.src = "./assets/images/slideshow/" + currentSlide.image;
+	bannerDescription.innerHTML = currentSlide.tagLine;
+	updateDots();
+}
+
+// Fonction qui met à jour les points de la bannière en fonction de l'index de la diapositive
+function updateDots() {
+	const dotElements = banner.querySelectorAll(".dot");
+	for (var i = 0; i < dotElements.length; i++) {
+		if (i === currentSlideIndex) {
+			dotElements[i].classList.add("dot_selected");
+		} else {
+			dotElements[i].classList.remove("dot_selected");
+		}
+	}
+}
 // Ajouter un événement de clic à la flèche gauche
-arrowLeft.addEventListener("click", function(){
+arrowLeft.addEventListener("click", function () {
+	currentSlideIndex = (currentSlideIndex + slides.length - 1) % slides.length;
+	updateBanner();
 	console.log("La flèche gauche a été cliqué !");
 	console.log(currentSlideIndex);
 });
 
 // Ajouter un événement de clic à la flèche droite
-arrowRight.addEventListener("click", function(){
+arrowRight.addEventListener("click", function () {
+	currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+	updateBanner();
 	console.log("La flèche droite a été cliqué !");
 	console.log(currentSlideIndex);
 });
+
+
 
 
